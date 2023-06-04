7fcb456cabe71d7a2603a3f0e1e42abcd60abd58
FabianRath <94621856+FabianRath@users.noreply.github.com>
2023-06-04 17:52:41 +0200

    inital

---
+++ content.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Select and remove elements by class name
+    const elements = document.getElementsByClassName('style-scope ytd-rich-section-renderer');
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].remove();
+    }
+  });
+  
