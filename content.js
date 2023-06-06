e6ebc9d74250d437af67f6d40d650ed2772473de
FabianRath <fabiratiac@gmail.com>
2023-06-06 17:34:32 +0200

    fixed deletion and added icons

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
-window.onload = () => {
-    // Select and remove elements by class name
-    const elements = document.getElementsByClassName('style-scope ytd-rich-section-renderer');
+function removeElements() {
+  const elements = document.getElementsByClassName('style-scope ytd-rich-section-renderer');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].remove();
+  }
+}
 
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].remove();
-    }
-  };
+// Run the function every 10 seconds (adjust the interval as needed)
+setInterval(removeElements, 100);
