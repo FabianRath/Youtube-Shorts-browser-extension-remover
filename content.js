b3ff446d4519b2c8a96763e3d3e8ae982669e446
FabianRath <fabiratiac@gmail.com>
2023-06-06 13:33:09 +0200

    fixed

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.onload = () => {
     // Select and remove elements by class name
     const elements = document.getElementsByClassName('style-scope ytd-rich-section-renderer');
 
     for (let i = 0; i < elements.length; i++) {
       elements[i].remove();
     }
-  });
+  };
